97a6fcb9b04261077d6ef7821ff09127fe8eede9
Sebastián López Montenegro <110605842+Sxl07@users.noreply.github.com>
2024-06-06 20:51:43 -0500

    Delete DataServer directory

--- DataServer/Random/i_random_number.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class IRandomNumber(ABC):
-    @abstractmethod
-    def generateNumbers(self,cant,min,max):
-        pass
--- DataServer/Random/randomUniformDistribution.py
+++
@@ -1,7 +0,0 @@
-from .i_random_number import IRandomNumber
-import numpy # type: ignore
-
-class RandomUniformDistribution(IRandomNumber):
-    def generateNumbers(self, cant, minlimit, maxlimit):
-        numbers = numpy.random.uniform(minlimit,maxlimit,cant).astype(int)
-        return numbers.tolist()
